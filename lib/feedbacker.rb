fd2c8404123c11750bac41b91da4e912bdc18113
Colton <coltonj@hranswerlink.com>
2013-11-12 16:04:55 -0800

    Corrected assets precompile

--- lib/feedbacker.rb
+++ lib/feedbacker.rb
@@ -3,10 +3,14 @@ Dir[File.join(File.dirname(__FILE__), '/feedbacker/*.rb')].each {|file| require 
 module Feedbacker
   class Engine < ::Rails::Engine
     isolate_namespace Feedbacker
-    config.assets.precompile += %w(feedbacker.js feedbacker.css)
+
     config.active_record.observers = :remark_observer
     rake_tasks do
       Dir[File.join(File.dirname(__FILE__),'tasks/*.rake')].each { |f| load f }
+    end
+
+    initializer "feedbacker.assets.precompile" do |app|
+      app.config.assets.precompile += %w(feedbacker.js feedbacker.css)
     end
   end
 end
